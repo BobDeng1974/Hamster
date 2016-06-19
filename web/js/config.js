3cd2c1b15705c7bb8a726505c10956ee4b0d76fb
Coselding <1098129797@qq.com>
2016-06-19 21:28:38 +0800

    配置文件地址bug

--- web/js/config.js
+++ web/js/config.js
@@ -48,5 +48,5 @@ CKEDITOR.editorConfig = function( config ) {
     config.font_names="宋体/宋体;黑体/黑体;仿宋/仿宋_GB2312;楷体/楷体_GB2312;隶书/隶书;幼圆/幼圆;微软雅黑/微软雅黑;" + config.font_names;
 
     //后台图片上传路径
-    config.filebrowserUploadUrl="/MyBlog/manage/uploadImage.action";
+    config.filebrowserUploadUrl="/Hamster/manage/uploadImage.action";
 };
